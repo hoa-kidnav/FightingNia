6bfacb7e948482ff4bebe5e30f449fd6ba06d5a0
hoa-kidnav <hoangbmt522@gmail.com>
2023-12-23 11:15:27 +0700

    first commit game

---
+++ index.js
@@ -0,0 +1,41 @@
+// profect setup
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+canvas.width = 1024;
+canvas.height = 576;
+
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+class Sprite {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+  }
+  //   paint red
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, 50, 150);
+  }
+  //   update
+  update() {
+    this.draw();
+
+    this.position.y += 10;
+  }
+}
+// local
+const player = new Sprite({
+  position: { x: 0, y: 0 },
+  velocity: { x: 0, y: 0 },
+});
+player.draw();
+const enemy = new Sprite({
+  position: { x: 400, y: 100 },
+  velocity: { x: 0, y: 0 },
+});
+enemy.draw();
+function animate() {
+  window.requestAnimationFrame(animate);
+  console.log("go");
+}
+animate();
